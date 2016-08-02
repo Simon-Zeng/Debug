afdf000bb9207abfb80bee025e13d6af76eba0d9
wzg <wzg@wzgdeMac-mini.local>
2016-08-02 19:13:20 +0800

    添加任务分发器

---
+++ TestTabelController/TestTabelController/Task/taskDispatcher.h
@@ -0,0 +1,14 @@
+//
+//  taskDispatcher.h
+//  TestTabelController
+//
+//  Created by wzg on 16/8/2.
+//  Copyright © 2016年 王智刚. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface taskDispatcher : NSObject
+
+
+@end
---
+++ TestTabelController/TestTabelController/Task/taskDispatcher.m
@@ -0,0 +1,17 @@
+//
+//  taskDispatcher.m
+//  TestTabelController
+//
+//  Created by wzg on 16/8/2.
+//  Copyright © 2016年 王智刚. All rights reserved.
+//
+
+#import "taskDispatcher.h"
+
+@interface taskDispatcher()
+//@property (nonatomic, strong)NSOperationQueue *<#name#>;
+@end
+
+@implementation taskDispatcher
+
+@end
